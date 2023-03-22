1bf012205e145dac1e5f8eb89a9c2fd905dad4fc
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2023-03-22 21:39:54 +0530

    uploading files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA7SgdZVxx6DQMhz9ZwS75YKHS4-G8KTrE",
+  authDomain: "netflix-clone-19dc5.firebaseapp.com",
+  projectId: "netflix-clone-19dc5",
+  storageBucket: "netflix-clone-19dc5.appspot.com",
+  messagingSenderId: "78157840859",
+  appId: "1:78157840859:web:0c38bc570e5d9bf27367fe",
+};
