7cb3202f1342f82dc473ae1b4ab57e495e60a61a
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2023-03-26 00:16:59 +0530

    created row component and fetch the movies in it according to the category

--- src/Banner.js
+++ src/Banner.js
@@ -27,32 +27,28 @@ function Banner() {
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + "..." : string;
   }
+
+  // const base_url = "https://image.tmdb.org/t/p/original/";
+
   return (
     <header
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url('https://t4.ftcdn.net/jpg/04/30/78/71/360_F_430787191_PUREgjGnYQY5G2myi3PTgyZ4rm1XfqEu.jpg')`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}}")`,
         backgroundPosition: "center center",
       }}
     >
       <div className="banner__contents">
-        <h1 className="banner__title">Movie Name</h1>
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
         <h1 className="banner__description">
-          {truncate(
-            `Movie Description Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. A nihil ullam maxime aut reprehenderit error ipsa magni
-          obcaecati molestias modi ducimus nisi voluptates, eos deleniti
-          possimus tenetur quibusdam dicta laudantium? Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Odio porro, accusamus iste sint aut
-          adipisci voluptate est tempora dolore ducimus beatae mollitia
-          assumenda amet minus numquam obcaecati voluptatum vero explicabo?`,
-            150
-          )}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
 
