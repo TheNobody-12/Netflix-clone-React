3cb4e86a8e2c9f38679a56f2813b5de616c59296
Veerangi18 <veerangimehta18@gmail.com>
2023-09-10 18:10:48 +0530

    updates

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -19,7 +19,6 @@ function ProfileScreen() {
           />
           <div className="profileScreen__details">
             <h2>{user.email}</h2>
-<<<<<<< HEAD
             <div className="profileScreen__plans">
               <h3>Plans</h3>
 
@@ -31,15 +30,13 @@ function ProfileScreen() {
                 Sign Out
               </button>
             </div>
-=======
             <div className="profileScreen__plans"></div>
-            <button
+            {/* <button
               onClick={() => auth.signOut()}
               className="profileScreen__signOut"
             >
               Sign Out
-            </button>
->>>>>>> 25acfde1a941a08304f9a59fb7495d9d9c79378f
+            </button> */}
           </div>
         </div>
       </div>
